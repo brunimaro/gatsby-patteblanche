d190794123066fbce9d497924d13cfae9dabd07e
brunimaro <brunowhereentreprise@gmail.com>
2020-09-17 20:40:03 +0200

    correction font Lora

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,10 +43,6 @@ module.exports = {
       options: {
         fonts: [
           {
-            family: `Lora`,
-            variants: [`400`, `500`, `600`, `700`]
-          },
-          {
             family: `Poppins`,
             variants: [`300`, `400`, `500`, `600`, `700`]
           },
